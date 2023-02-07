9279864919608a2800b569ebcb9ffd15f2c2e95a
AlexanderKorytin <al.corytin@yandex.ru>
2023-02-07 18:03:37 +0300

    написан ввод числа людей

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,42 @@
-// dev branch for Y.Practicum
-public class Main {
+import java.util.Scanner;
+public class Main
+{
 
     public static void main(String[] args) {
-        // ваш код начнется здесь
-        // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
-        System.out.println("Привет Мир");
+        print("Введие количество человек оплачивающих счет:");
+        int numberPeople =  waitInt(1,100);
+        print("Число гостей — " + numberPeople);
+    }
+
+    public static int waitInt (int numPeopleMin, int numPeopleMax)
+    {
+        int numberPeople = 0;
+        String message = "Введите целое число > %d и < %d";
+        Scanner scanner = new Scanner(System.in);
+        while (true)
+        {
+            if (scanner.hasNextInt())
+            {
+                numberPeople = scanner.nextInt();
+                if(numberPeople>numPeopleMin&&numberPeople<numPeopleMax)
+                {
+                    scanner.close();
+                    return numberPeople;
+                }
+                else
+                {
+                    print(String.format(message, numPeopleMin, numPeopleMax));
+                }
+            }
+            if (!scanner.hasNextInt()) {
+                print(String.format(message, numPeopleMin, numPeopleMax));
+                scanner.next();
+            }
+        }
+    }
+
+    public static void print(String massege)
+    {
+        System.out.println(massege);
     }
 }
