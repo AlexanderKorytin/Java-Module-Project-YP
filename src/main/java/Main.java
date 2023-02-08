ff689bf1633e59e6789caf9c1f12f2b939b691d3
AlexanderKorytin <al.corytin@yandex.ru>
2023-02-08 18:22:13 +0300

    закончен, но класс MenuBar во вкладке Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,28 +1,43 @@
 import java.util.Scanner;
 public class Main
 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         print("Введие количество человек оплачивающих счет:");
-        int numberPeople = waitInt(1, 100);
-        String nameList, message;
-        float priceProd;
+        int numberPeople = waitInt(1, 100); // запрашиваем
+        // и получаем от пользователя количество человек оплачивающих счет
+        String nameList, message, rub = "";
+        float priceProd =0.0f, resultRub = 0.0f;
         MenuBar list = new MenuBar("",0.0f);
         Scanner scan = new Scanner(System.in);
         print("Введите наименование товара:");
+        // далее производим вычисление и выводим результат
         while (true)
         {
-            nameList = list.writeMenu();
+            nameList = list.writeMenu(); // заполняем строку наименованиями
+            // введенных пользователем товаров
             print("Введите сумму товара в виде 00,00:");
-            priceProd = list.priceSum();
-            if(scan.nextLine().equalsIgnoreCase("Завершить"))
-            {break;}
-            else{print("Введите наименование товара:");}
-        }
-        print("Число гостей — " + numberPeople);
-        message = "Длбавленные товары:\n %s. \nНаобщую сумму: %.2f.\n по %.2f %s с каждого";
-        print(String.format(message, nameList, priceProd, priceProd/numberPeople, "рубля"));
-    }
+            priceProd = list.priceSum(); // запрашиваем цену товара и прибавляем ее к
+            // сумме ранее введенных цен
 
+            if(scan.nextLine().equalsIgnoreCase("Завершить"))
+            {
+              break;
+            }
+            else
+            {
+              print("Введите наименование товара:");
+            }
+        }
+        resultRub = priceProd/numberPeople;
+        rub = list.printResult(resultRub); // определяем окончание слова "рубль"
+        // в зависимости от полученной суммы товаров
+        message = "Добавленные товары:\n %s. \nНаобщую сумму: %.2f.\n%.2f %s с каждого";
+        print(String.format(message, nameList, priceProd, resultRub, rub));
+
+    }
+// далее метод запрашивающий целочисленное значение >numPeopleMin и <numPeopleMax
+    // и игнорирующий введение любых других символов, строк и т.д.
     public static int waitInt (int numPeopleMin, int numPeopleMax)
     {
         int numberPeople = 0;
@@ -48,7 +63,7 @@ public class Main
             }
         }
     }
-
+// далее метод вывода сообщения в консоль (лень)
     public static void print(String massege)
     {
         System.out.println(massege);
@@ -56,39 +71,72 @@ public class Main
     // объявляем новый класс - строка меню(прайса)
     public static class MenuBar
     {
-    float priceDish;
-    String nameDish;
+        float priceDish;
+        String nameDish;
 
-    public MenuBar(String nameDish, float priceDish)
-    {
-        this.nameDish = nameDish;
-        this.priceDish = priceDish;
-    }
-
-    public String writeMenu()
-     {
-         Scanner scan = new Scanner(System.in);
-         nameDish = nameDish + "\n" + scan.nextLine();
-         return nameDish;
-      }
-     public float priceSum()
-      {
-          Scanner scan = new Scanner(System.in);
-          while (true)
-          {
-              if (scan.hasNextDouble())
-              {
-                  priceDish = priceDish + scan.nextFloat();
-                  System.out.println("Товар успешно добавлен.\nдобавить еще товар? - введите любой символ!\n\nЧтобы завершить введите \"Завершить\"");
-                  return priceDish;
-              }
-              else
-              {
-                  System.out.println("Введите сумму товара в виде 00,00");
-                  scan.nextLine();
-              }
-          }
-      }
+        public MenuBar(String nameDish, float priceDish)
+        {
+            this.nameDish = nameDish;
+            this.priceDish = priceDish;
+        }
+        // далее метод определяющий окончание слова "рубль"
+        // в зависимости от полученной суммы товаров
+        public String printResult(float result)
+        {
+            String rubl = "Рублей";
+            if(result%100<=11&&result%100>=19)
+            {
+                rubl = "рублей";
+            }
+            switch(((int)result)%10)
+            {
+                case 0:
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                case 9:
+                    rubl = "рублей";
+                    break;
+                case 1:
+                    rubl = "рубль";
+                    break;
+                case 2:
+                case 3:
+                case 4:
+                    rubl = "рубля";
+                    break;
+            }
+            return rubl;
+        }
+        //далее метод заполняющий строку наименованиями
+        // введенных пользователем товаров
+        public String writeMenu()
+        {
+            Scanner scan = new Scanner(System.in);
+            nameDish = nameDish + "\n" + scan.nextLine();
+            return nameDish;
+        }
+        // далее метод запрашивающий цену товара и прибавляющий ее к
+        // сумме ранее введенных цен
+        public float priceSum()
+        {
+            Scanner scan = new Scanner(System.in);
+            while (true)
+            {
+                if (scan.hasNextDouble())
+                {
+                    priceDish = priceDish + scan.nextFloat();
+                    System.out.println("Товар успешно добавлен.\nДобавить еще товар? - введите любой символ!\n\nЧтобы завершить введите \"Завершить\"");
+                    return priceDish;
+                }
+                else
+                {
+                    System.out.println("Введите сумму товара в виде 00,00");
+                    scan.nextLine();
+                }
+            }
+        }
     }
 }
 
